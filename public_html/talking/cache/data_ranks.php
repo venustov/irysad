dab6d11d2e93b2d5683a708745a8496287927baf
venustov <venustov@gmail.com>
2020-08-13 12:09:27 +0300

    Второй коммит

--- public_html/talking/cache/data_ranks.php
+++
@@ -1,7 +0,0 @@
-<?php
-$expired = (time() > 1501875677) ? true : false;
-if ($expired) { return; }
-
-$data =  unserialize('a:1:{s:7:"special";a:1:{i:1;a:2:{s:10:"rank_title";s:26:"Администратор";s:10:"rank_image";s:0:"";}}}');
-
-?>
--- public_html/talking/cache/data_word_censors.php
+++
@@ -1,7 +0,0 @@
-<?php
-$expired = (time() > 1501875654) ? true : false;
-if ($expired) { return; }
-
-$data =  array();
-
-?>
